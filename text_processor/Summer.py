e0d8cc52d52daadd4a4824cbf9e551d953c02835
Kotorinyanya <cnsrt8613@gmail.com>
2018-05-02 23:29:03 +0800

    nothing changed

--- text_processor/Summer.py
+++ text_processor/Summer.py
@@ -38,4 +38,5 @@ def sum_form_string(string, language="english", sentences_cout=100):
 
 if __name__ == "__main__":
     text_file = "document.txt"
-    keyphrases = sum_form_file(text_file)
+    keyphrases = sum_form_string(string='t first I was upset when they told me we were moving to some little town out in the Ozarks. I remember staring at my dinner plate while I listened to my sister throw a temper tantrum unbefitting of a 14 year old honors student. She cried, she pleaded, and then she cursed at my parents. She threw a bowl at my dad and told him it was all his fault. Mom told Whitney to calm down but she stormed off, slamming every door in the house on the way to her room.')
+    print()
