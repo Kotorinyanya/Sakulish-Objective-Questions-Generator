4c6a6d61efa197c9ba9810820a814cceb8f580f0
Kotorinyanya <cnsrt8613@gmail.com>
2018-05-05 00:56:51 +0800

    edited QuestionBuilder

--- text_processor/code/QuestionBuilder.py
+++ text_processor/code/QuestionBuilder.py
@@ -1,8 +1,9 @@
 from NLPMainHandler import NLPMainHandler
-from Summer import *
+from Summer import sum_form_url, sum_form_file, sum_form_string
 
 import random
-
+import codecs
+import json
 
 class QuestionBuilder:
     def __init__(self, url=None, file=None, string=None):
@@ -16,18 +17,14 @@ class QuestionBuilder:
         else:
             raise Exception("input url/file/string required!")
 
-        self.NER_tags_to_append = {
-            'ORGANIZATION': ['Asda', 'CMA', 'Tesco', 'Walmart', 'Sainsbury', 'Lidl', 'BBC News'],
-            'DATE': ['2016', 'the autumn of 2019', 'current', 'the weekend', 'currently', '1999'],
-            'COUNTRY': ['UK', 'US', 'China', 'Japan'],
-            'TITLE': ['chairman', 'mobile', 'chief executive', 'leader', 'business secretary', 'boss', 'general',
-                      'analyst', 'director', 'retailer'],
-            'PERSON': ['Vince Cable', 'Mike Cherry', 'Mike Coupe', 'Coupe', 'Argos', 'Dresser', 'Nick Bubb',
-                       'Rebecca Long-Bailey', 'Steve Dresser'], 'ORDINAL': ['second and third'],
-            'NUMBER': ['1', '3', '4', '2', '5', 'two', 'one'], 'PERCENT': ['10 %', '42 %', '30 %'],
-            'IDEOLOGY': ['Liberal Democrat', "I don't know", "I don't know either"],
-            'LOCATION': ['Chengdu', 'Tokyo', 'Beijing', 'Shanghai']
-        }
+        # build the complete set of NER tags
+        with open('resources/ner_tags.json', 'r') as infile:
+            self.NER_tags_to_append = json.load(infile)
+
+        # build the complete set of relations
+        with codecs.open('resources/properties-with-labels.txt', encoding='utf-8') as infile:
+            self._property2label = {l.split("\t")[0]: l.split("\t")[1].strip() for l in infile.readlines()}
+        self.relations_to_append = [relation for relation in self._property2label.values()]
 
         # NLP
         self.nlp_handler = NLPMainHandler(self.input_sentences)
@@ -55,7 +52,11 @@ class QuestionBuilder:
 
             relations_to_choose = list(choices_complete_set)
             if relations_to_choose.__len__() < 4:
-                raise Exception('relations extracted form text is not enough!')
+                try:
+                    relations_to_choose = list(set(relations_to_choose + self.relations_to_append))
+                except:
+                    print('relations extracted form text is not enough!')
+                    continue
             choice_A = relation[1]
             relations_to_choose.remove(choice_A)
             choice_B = random.choice(relations_to_choose)
@@ -84,21 +85,36 @@ class QuestionBuilder:
         #     sentences_to_use.append(chosen_sentence)
 
         for current_sentence in sentences_to_use:
-            for i in range(2, current_sentence.__len__()):
-                this_NER_tag = current_sentence[i][1]
-                if this_NER_tag is not 'O':
-                    this_tagged_word = current_sentence[i][0]
-                    m = i + 1
-                    while current_sentence[m][1] is this_NER_tag:
-                        this_tagged_word += ' ' + current_sentence[m][0]
-                        m += 1
-                    choices_complete_list = NER_tags[this_NER_tag].copy()
-                    if choices_complete_list.__len__() < 4:
+            current_word_i = 0
+            while current_word_i < current_sentence.__len__():
+                this_ner_tag = current_sentence[current_word_i][1]
+                this_tagged_word = current_sentence[current_word_i][0]
+                current_word_m = current_word_i + 1
+                current_word_i += 1
+                if this_ner_tag != 'O':
+                    if current_word_m >= current_sentence.__len__():
+                        break
+                    next_ner_tag = current_sentence[current_word_m][1]
+                    while next_ner_tag == this_ner_tag:
+                        this_tagged_word += ' ' + current_sentence[current_word_m][0]
+                        current_word_m += 1
+                        current_word_i += 1
+                        if current_word_m >= current_sentence.__len__():
+                            break
+                        next_ner_tag = current_sentence[current_word_m][1]
+                    choices_complete_list = NER_tags[this_ner_tag].copy()
+                    # TODO: This method guarantee that there's no repeated choices but reduce the number of subjects
+                    # if choices_complete_list.__len__() < 4:
+                    #     try:
+                    #         choices_complete_list = list(set(NER_tags[this_NER_tag] + NER_tags_to_append[this_NER_tag]))
+                    #     except:
+                    #         # TODO: extend NER_tags_to_append by crawling passages
+                    #         pass
+                    # TODO: This mothod do not guarantee that there's no repeated choices
+                    while choices_complete_list.__len__() < 4:
                         try:
-                            for x in NER_tags_to_append[this_NER_tag]:
-                                choices_complete_list.append(x)
+                            choices_complete_list += NER_tags[this_ner_tag] + NER_tags_to_append[this_ner_tag]
                         except:
-                            # TODO: extend NER_tags_to_append to avoid key error
                             pass
                     try:
                         choice_A = this_tagged_word
@@ -109,23 +125,34 @@ class QuestionBuilder:
                         choices_complete_list.remove(choice_C)
                         choice_D = random.choice(choices_complete_list)
                         choices_complete_list.remove(choice_D)
-                    except:
+                    except Exception as e:
+                        print(e)
+                        print('the complete set of NER tags is not enough!')
+                        print(NER_tags[this_ner_tag])
+                        print(choices_complete_list)
+                        print(choice_A)
                         break
-                        # raise Exception('NER tags is not enough!')
                     question = ''
-                    for cursor in range(2, current_sentence.__len__()):
+                    cursor = 0
+                    while cursor < current_sentence.__len__():
                         cursor_tagged_word = current_sentence[cursor][0]
                         cursor_tag = current_sentence[cursor][1]
                         next_cursor = cursor + 1
                         if next_cursor >= current_sentence.__len__():
-                            continue
-                        while current_sentence[next_cursor][1] is cursor_tag and cursor_tag is not 'O':
+                            break
+                        next_tag = current_sentence[next_cursor][1]
+                        while next_tag == cursor_tag:
                             cursor_tagged_word += ' ' + current_sentence[next_cursor][0]
                             next_cursor += 1
-                        if cursor_tagged_word is not this_tagged_word:
+                            cursor += 1
+                            if next_cursor >= current_sentence.__len__():
+                                break
+                            next_tag = current_sentence[next_cursor][1]
+                        if cursor_tagged_word != this_tagged_word:
                             question += cursor_tagged_word + ' '
-                        elif cursor_tagged_word is this_tagged_word:
+                        elif cursor_tagged_word == this_tagged_word:
                             question += '_______' + ' '
+                        cursor += 1
 
                     subject = {
                         'question': question,
@@ -159,6 +186,7 @@ class QuestionBuilder:
         }
         subjects_complete_list.append(subject)
         return subjects_complete_list
+
 
 if __name__ == '__main__':
     qe = QuestionBuilder(url='http://www.bbc.com/news/uk-43941624')
